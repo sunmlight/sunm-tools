c946ea2c6e96dfa8454c82e106ccde6e7e75973c
sunmlight <sunm8917@gmail.com>
2020-03-10 00:03:18 +0800

    get_wf_key

--- apps/warframe/helper.py
+++ apps/warframe/helper.py
@@ -1,5 +1,15 @@
 import requests
 from django.conf import settings
+import os
 
 def get_wf_key():
-    pass
+    WFAPI = settings.WFAPI
+    wfurl = WFAPI["BASE_URL"]
+    url = WFAPI["BASE_URL"] + "/connect/token"
+    data = {
+        "client_id": WFAPI["ClientId"],
+        "client_secret": WFAPI["ClientSecret"],
+        "grant_type": "client_credentials",
+    }
+    headers={'Content-Type':'application/x-www-form-urlencoded'}
+    req = requests.post(url, data=data, headers=headers)
