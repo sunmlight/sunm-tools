8e67068b04a14b08374ad56148869fbefcf415ff
sunmlight <sunm8917@gmail.com>
2020-03-13 00:36:48 +0800

    db url

--- SunmTools/settings.py
+++ SunmTools/settings.py
@@ -79,7 +79,10 @@ WSGI_APPLICATION = "SunmTools.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    "default": os.environ.get("DATABASE_URL"),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
 }
 
 # local
