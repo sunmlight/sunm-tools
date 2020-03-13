6fcd448c50968b30a18a635e76bd162f51a67baa
sunmlight <sunm8917@gmail.com>
2020-03-13 23:13:45 +0800

    static

--- SunmTools/urls.py
+++ SunmTools/urls.py
@@ -15,12 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf import settings
+from django.urls import re_path
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("sadmin/", admin.site.urls),
     path("wechat/", include("wechat.urls")),
     path("wf/", include("warframe.urls")),
     path("note/", include("note.urls")),
     path("notice/", include("notice.urls")),
 ]
+
+urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {
+            'document_root': settings.STATIC_ROOT,
+        }),
+    ]
