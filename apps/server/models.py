d15f73280abb998f83e189a49b4f79526d429a0f
sunmlight <sunm8917@gmail.com>
2020-03-21 18:41:36 +0800

    model-taskinfo

--- apps/server/models.py
+++ apps/server/models.py
@@ -11,3 +11,21 @@ class UserInfo(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class TaskInfo(models.Model):
+    task_id = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
+    run_type = models.IntegerField(default=0, choices=())
+    status = models.IntegerField(default=0)
+    result = models.CharField(max_length=50, blank=True, null=True)
+    script = models.TextField(blank=True, null=True, verbose_name="脚本")
+    count = models.IntegerField(default=0, verbose_name="总计")
+    last_run_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "TaskInfo"
+        verbose_name_plural = "TaskInfo"
+
+    def __str__(self):
+        return self.name
